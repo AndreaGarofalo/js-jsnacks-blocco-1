dae8a07b0ef1a0a6ae8e6daa7b00cbcded585761
AndreaGarofalo <andreasjobs94@outlook.it>
2022-12-13 18:15:34 +0100

    minor fix line 6

--- js/numbers.js
+++ js/numbers.js
@@ -3,7 +3,7 @@ console.log("JS OK");
 const finalNumbers = document.getElementById("target");
 let userNumbers = 0;
 
-for (i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   userNumbers += parseInt(prompt("scrivi un numero", 1).trim());
   if (isNaN(userNumbers)) {
     alert("I dati inseriti non sono corretti");
