4d3136af6f493c2ecbbb98962641afe6711ec254
AndreaGarofalo <andreasjobs94@outlook.it>
2022-12-13 19:58:40 +0100

    added track on script

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,4 +1,6 @@
 console.log("JS OK");
+// SNACK 1
+// Il software deve chiedere per 10 volte all’utente di inserire un numero. (qui potete usare un prompt). Il programma stampa la somma di tutti i numeri inseriti.
 
 const finalNumbers = document.getElementById("target");
 let userNumbers = 0;
