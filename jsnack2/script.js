9d4aab13a75e4c41deaf243e94238e781771f293
AndreaGarofalo <andreasjobs94@outlook.it>
2022-12-13 20:37:15 +0100

    jsnack2 finished

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,3 +1,33 @@
 console.log("JS OK");
 
+// SNACK 2
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, l’utente inserisce il suo nome tramite un input.comunicagli se può partecipare o no alla festa. (vietato usare includes() )
+
 const partecipant = ["Pippo", "Ciccio", "Gennarino", "Franco", "Pdor"];
+
+const nameField = document.getElementById("name");
+const button = document.getElementById("button");
+const partecipation = document.getElementById("target");
+
+button.addEventListener("click", function () {
+  const userName = nameField.value.trim();
+
+  if (!userName) {
+    partecipation.innerText = "Non hai compilato il form";
+    return;
+  }
+
+  let isAllowed = false;
+
+  for (let i = 0; i < partecipant.length; i++) {
+    const currentPartecipant = partecipant[i];
+    if (currentPartecipant === userName) {
+      isAllowed = true;
+    }
+  }
+  if (isAllowed) {
+    partecipation.innerText = "Benvenuto";
+  } else {
+    partecipation.innerText = "Non sei uno degli invitati";
+  }
+});
