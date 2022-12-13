81dca785054f94fd5fb7e25a5663e55477d82bbd
AndreaGarofalo <andreasjobs94@outlook.it>
2022-12-13 17:13:16 +0100

    first commit

---
+++ js/numbers.js
@@ -0,0 +1,12 @@
+console.log("JS OK");
+
+const finalNumbers = document.getElementById("target");
+let userNumbers = 0;
+
+for (i = 0; i < 10; i++) {
+  userNumbers += parseInt(prompt("scrivi un numero", 1).trim());
+
+  console.log(userNumbers);
+}
+
+finalNumbers.innerHTML = `<p>The sum of the numbers you have chosen is: <strong>${userNumbers}</strong></p>`;
