3c91330ecc17f49e1cfaddfe337c353016399ea8
AndreaGarofalo <andreasjobs94@outlook.it>
2022-12-13 17:24:23 +0100

    added validation and changed name of js file

--- js/numbers.js
+++ js/numbers.js
@@ -5,8 +5,10 @@ let userNumbers = 0;
 
 for (i = 0; i < 10; i++) {
   userNumbers += parseInt(prompt("scrivi un numero", 1).trim());
-
+  if (isNaN(userNumbers)) {
+    alert("I dati inseriti non sono corretti");
+    break;
+  }
   console.log(userNumbers);
+  finalNumbers.innerHTML = `<p>The sum of the numbers you have chosen is: <strong>${userNumbers}</strong></p>`;
 }
-
-finalNumbers.innerHTML = `<p>The sum of the numbers you have chosen is: <strong>${userNumbers}</strong></p>`;
